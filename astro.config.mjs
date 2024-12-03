fefd42f7c2331af4e656c3e99697792abdb7aefd
Joe Linogao <linogaoj@tcd.ie>
2024-12-03 20:14:24 +0000

    added all deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config'
 
 import node from '@astrojs/node'
+
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +12,10 @@ export default defineConfig({
     adapter: node({
         mode: 'standalone',
     }),
+
     server: {
         host: '0.0.0.0',
     },
+
+    integrations: [tailwind()],
 })
