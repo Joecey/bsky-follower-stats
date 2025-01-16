5210eb5ca458575b6d4146706bb7933abc198c9c
Joe Linogao <linogaoj@tcd.ie>
2025-01-16 22:38:59 +0000

    input field navigating to dynamic page + added clientrouter

--- src/components/WelcomeSection/BlueskyHandleForm.tsx
+++ src/components/WelcomeSection/BlueskyHandleForm.tsx
@@ -1,23 +1,43 @@
 import { Field } from '@ark-ui/react/field'
 import { useState } from 'react'
+import trim from 'lodash/trim'
+import toLower from 'lodash/toLower'
 
 export const BlueskyHandleForm = () => {
-    const [username, setUserName] = useState('')
+    const defaultUserName = ''
+    const [username, setUserName] = useState(defaultUserName)
+    const [buttonDisabled, setButtonDisabled] = useState(true)
+
+    const handleInputField = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setButtonDisabled(
+            toLower(trim(e.target.value).replace(/@/g, '')).length === 0 ? true : false
+        )
+        setUserName(`${toLower(trim(e.target.value).replace(/@/g, ''))}`)
+    }
 
     return (
         <div id="useranme-form" className="w-full">
             <Field.Root className="flex flex-col">
-                <Field.Label className="py-1 text-xl">
+                <Field.Label className="py-1 text-lg">
                     Enter your @bluesky handle to learn more about your followers!
                 </Field.Label>
-                <Field.Input className="rounded-sm text-2xl" />
+                <Field.Input
+                    className={`rounded-md p-1 text-2xl shadow-sm ${username.replace('@', '') === '' ? 'text-gray-400' : 'text-black'} transition-colors`}
+                    onChange={handleInputField}
+                    value={username ? `@${username}` : '@'}
+                />
                 <Field.HelperText className="text-sm text-gray-600">
                     Make sure that you have a valid bluesky handle!
                 </Field.HelperText>
-                <Field.ErrorText>Error Info</Field.ErrorText>
             </Field.Root>
-
-            {username && <p>Welcome {username}</p>}
+            <a href={`/followers/${username}`}>
+                <button
+                    className="mt-4 rounded-full bg-sky-500 px-4 py-2 text-base text-white transition-colors hover:bg-sky-700 disabled:cursor-not-allowed disabled:bg-gray-400"
+                    disabled={buttonDisabled}
+                >
+                    Search user
+                </button>
+            </a>
         </div>
     )
 }
