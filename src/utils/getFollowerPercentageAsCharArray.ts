e09c1d574493f3e8e287fef02f93fe0046b252ed
Joe Linogao <linogaoj@tcd.ie>
2025-01-20 20:37:34 +0000

    calcualting percentage and converting to string array

---
+++ src/utils/getFollowerPercentageAsCharArray.ts
@@ -0,0 +1,12 @@
+export const getFollowerPercentageAsCharArray = ({
+    userFollowers,
+    totalUsers,
+}: {
+    userFollowers: number
+    totalUsers: number
+}): string[] => {
+    const maxArraySize = 10
+    const percentage = ((userFollowers / totalUsers) * 100).toString()
+    const percentageDisplayArray = percentage.split('', maxArraySize)
+    return percentageDisplayArray
+}
