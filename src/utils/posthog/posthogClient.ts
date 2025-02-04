a010be70234e0f65c46e3cc6af7014e528882f8a
Joe Linogao <linogaoj@tcd.ie>
2025-02-04 18:36:23 +0000

    Revert "Revert "connected post hog""
    This reverts commit 76d920019d775ee7a208931a071696a1fc5bbc00.

---
+++ src/utils/posthog/posthogClient.ts
@@ -0,0 +1,9 @@
+import { PostHog } from 'posthog-node'
+
+export const PostHogClient = () => {
+    const posthogClient = new PostHog(import.meta.env.SECRET_POSTHOG_KEY, {
+        host: import.meta.env.SECRET_POSTHOG_HOST,
+    })
+
+    return posthogClient
+}
